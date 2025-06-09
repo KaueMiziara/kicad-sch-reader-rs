c4996165cbe84b3f88af6c3c28400faa52599762
Kaue Miziara <kauemiziara@gmail.com>
2025-06-09 01:18:52 -0300

    build: add clap as dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+/// Clap test program
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Hello, {}!", args.name);
 }
